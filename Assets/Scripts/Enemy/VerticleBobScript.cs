c44e29ca4ac99116cd6f455e3d66cff170a03fa0
SeabassOlko <63417711+SeabassOlko@users.noreply.github.com>
2024-10-01 22:11:50 -0500

    Fixed Verticle Bob for balloons
    Summary
    - Fine tuned the bobbing script to allow for adjustment and application to multiple things
    - Added the bobbing script to the three main balloons

--- Assets/Scripts/Enemy/VerticleBobScript.cs
+++ Assets/Scripts/Enemy/VerticleBobScript.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class VerticleBobScript : MonoBehaviour
 {
-    [SerializeField] float bobAmount = 1f;
+    [SerializeField] float bobAmount = 0.5f;
+
+    float originalYPos;
+
+    void Start()
+    {
+        originalYPos = transform.position.y - bobAmount;    
+    }
     // Update is called once per frame
     void Update()
     {
-        float yPos = 4.2f - Mathf.PingPong(Time.time, bobAmount);
+        Debug.Log("Original y pos: " + originalYPos + " bob mafs: " + (Mathf.Cos(Time.time) * bobAmount));
+        float yPos = originalYPos + (Mathf.Cos(Time.time) * bobAmount);
         transform.position = new Vector3(transform.position.x, yPos, transform.position.z);       
     }
 }
