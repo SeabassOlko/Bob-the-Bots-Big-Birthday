468d038c2c0784f91e3fec182fd6ba8dfcd2c1de
Wesley Klawitter <wesman2007@hotmail.com>
2024-10-03 15:56:39 -0400

    fdfdfd

--- Assets/Scripts/PartyCrasherSpawner.cs
+++ Assets/Scripts/PartyCrasherSpawner.cs
@@ -17,10 +17,7 @@ public class PartyCrasherSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            SpawnPartyCrashers();
-        }
+        
     }
 
     public void SpawnPartyCrashers()
