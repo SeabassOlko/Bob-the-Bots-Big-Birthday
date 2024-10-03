ff82c1770d55dc9965399316f97f1615587fc2d1
JAHHH204 <alexander.baron@torontofilmschool.ca>
2024-10-03 17:25:33 -0500

    credit button

---
+++ Assets/Scripts/Misc/CreditButton.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CreditButton : MonoBehaviour
+{
+    SaveLoad saveLoad;
+    public GameObject pauseCanvas;
+   
+    public Button quitButton;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        quitButton.onClick.AddListener(quitGame);
+        pauseCanvas.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+
+
+    void quitGame()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
